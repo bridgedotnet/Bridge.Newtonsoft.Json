f9a2bf9de4ad48472203eee8b46a541fcd050da2
LeonidVeriga <leonid@object.net>
2017-06-22 22:55:52 +0300

    Additional Client tests

--- Tests/Newtonsoft.Json.Tests/Issues/0100/Case008.cs
+++ Tests/Newtonsoft.Json.Tests/Issues/0100/Case008.cs
@@ -13,10 +13,32 @@ namespace Newtonsoft.Json.Tests.Issues
     {
         public sealed class ApiResponse<T>
         {
+            public T Value;
         }
 
         public sealed class PageEditData
         {
+            public int Data
+            {
+                get; set;
+            }
+        }
+
+        public sealed class ApiResponse<T, K>
+        {
+            public T Value1;
+            public K Value2
+            {
+                get; set;
+            }
+        }
+
+        public sealed class PageEditData<T>
+        {
+            public T Data
+            {
+                get; set;
+            }
         }
 
         [Test]
@@ -28,16 +50,68 @@ namespace Newtonsoft.Json.Tests.Issues
             };
 
             var x = new ApiResponse<PageEditData>();
+
+            x.Value = new PageEditData()
+            {
+                Data = 7
+            };
+
             var json = JsonConvert.SerializeObject(x, settings);
 
-            Assert.AreEqual("{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+ApiResponse`1[[Newtonsoft.Json.Tests.Issues.Case8+PageEditData, Newtonsoft.Json.Tests]], Newtonsoft.Json.Tests\"}", json);
+            Assert.AreEqual("{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+ApiResponse`1[[Newtonsoft.Json.Tests.Issues.Case8+PageEditData, Newtonsoft.Json.Tests]], Newtonsoft.Json.Tests\",\"Value\":{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+PageEditData, Newtonsoft.Json.Tests\",\"Data\":7}}", json);
 
             object result = JsonConvert.DeserializeObject<ApiResponse<PageEditData>>(
                 json,
                 settings
             );
 
-            Assert.True(result is ApiResponse<PageEditData>);
+            Assert.NotNull(result);
+
+            var typedResult = result as ApiResponse<PageEditData>;
+            Assert.NotNull(typedResult);
+
+            Assert.AreEqual(7, typedResult.Value.Data);
+        }
+
+        [Test]
+        public static void TestGenericTypeHandlingMoreGenericLevel()
+        {
+            var settings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
+            var x = new ApiResponse<PageEditData<string>, PageEditData<PageEditData>>();
+
+            x.Value1 = new PageEditData<string>
+            {
+                Data = "Hi"
+            };
+
+            x.Value2 = new PageEditData<PageEditData>
+            {
+                Data = new PageEditData
+                {
+                    Data = 8
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(x, settings);
+
+            Assert.AreEqual("{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+ApiResponse`2[[Newtonsoft.Json.Tests.Issues.Case8+PageEditData`1[[System.String, mscorlib]], Newtonsoft.Json.Tests],[Newtonsoft.Json.Tests.Issues.Case8+PageEditData`1[[Newtonsoft.Json.Tests.Issues.Case8+PageEditData, Newtonsoft.Json.Tests]], Newtonsoft.Json.Tests]], Newtonsoft.Json.Tests\",\"Value1\":{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+PageEditData`1[[System.String, mscorlib]], Newtonsoft.Json.Tests\",\"Data\":\"Hi\"},\"Value2\":{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+PageEditData`1[[Newtonsoft.Json.Tests.Issues.Case8+PageEditData, Newtonsoft.Json.Tests]], Newtonsoft.Json.Tests\",\"Data\":{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case8+PageEditData, Newtonsoft.Json.Tests\",\"Data\":8}}}", json);
+
+            object result = JsonConvert.DeserializeObject<ApiResponse<PageEditData<string>, PageEditData<PageEditData>>>(
+                json,
+                settings
+            );
+
+            Assert.NotNull(result);
+
+            var typedResult = result as ApiResponse<PageEditData<string>, PageEditData<PageEditData>>;
+            Assert.NotNull(typedResult);
+
+            Assert.AreEqual("Hi", typedResult.Value1.Data);
+            Assert.AreEqual(8, typedResult.Value2.Data.Data);
         }
     }
 }
