b247d8b5a012f700227c4ce806953a957525d358
Fabrício Zimmerer Murta <fabricio@object.net>
2017-11-23 17:37:16 -0200

    Updates version to 1.4.0.

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2017 Object.NET, Inc.")]
