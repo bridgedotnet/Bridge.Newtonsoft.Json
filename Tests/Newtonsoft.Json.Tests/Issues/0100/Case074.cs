79517b1b99ddaecdb37136f21aaf086b9f1d0e4f
Fabrício Zimmerer Murta <fabricio@object.net>
2017-12-20 20:52:26 -0200

    Removes non-working scenarios from case74 unit tests.
    The broken tests do not directly affect the fix in this issue, so a new
    one was created to handle the problem (76).

--- Tests/Newtonsoft.Json.Tests/Issues/0100/Case074.cs
+++ Tests/Newtonsoft.Json.Tests/Issues/0100/Case074.cs
@@ -49,15 +49,6 @@ namespace Newtonsoft.Json.Tests.Issues
 
             Assert.AreEqual("{\"Value\":1}", Serialize(key, false), "Object serialized correctly.");
             Assert.AreEqual("{\"1\":\"None\"}", Serialize(listingLevelNames, false), "Custom dictionary serialized correctly.");
-
-            Assert.AreEqual("{\"$type\":\"Newtonsoft.Json.Tests.Issues.Case74+MyKey, Newtonsoft.Json.Tests\",\"Value\":1}", Serialize(key, true), "Object serialized correctly (detailed).");
-            Assert.AreEqual("{\"$type\":\"System.Collections.Generic.Dictionary`2[[Newtonsoft.Json.Tests.Issues.Case74+MyKey, Newtonsoft.Json.Tests],[System.String, mscorlib]], mscorlib\",\"1\":\"None\"}", Serialize(listingLevelNames, true), "Custom dictionary serialized correctly (detailed).");
-
-            var dict = new Dictionary<int, string>();
-            dict.Add(5, "five");
-
-            Assert.AreEqual("{\"5\":\"five\"}", Serialize(dict, false), "Basic dictionary serialized correctly.");
-            Assert.AreEqual("{\"$type\":\"System.Collections.Generic.Dictionary`2[[System.Int32, mscorlib],[System.String, mscorlib]], mscorlib\",\"5\":\"five\"}", Serialize(dict, true), "Basic dictionary serialized correctly (detailed).");
         }
     }
 }
