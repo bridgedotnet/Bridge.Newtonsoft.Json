ca273862c3d7ab9ffa2b9c5efd17ca8cbe91b584
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-16 20:51:30 -0200

    Updates version to 1.6.0 and Bridge references to 16.8.0.

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
-[assembly: AssemblyCopyright("Copyright 2008-2017 Object.NET, Inc.")]
+[assembly: AssemblyCopyright("Copyright 2008-2018 Object.NET, Inc.")]
