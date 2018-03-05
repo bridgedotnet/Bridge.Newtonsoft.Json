72f55eb73903b2e07ed17771401e8049a1b641c0
Fabrício Zimmerer Murta <fabricio@object.net>
2018-03-05 14:58:45 -0300

    Refreshes output files with Bridge version changes.

--- Tests/Runner/General/code.js
+++ Tests/Runner/General/code.js
@@ -3,7 +3,7 @@
  * @version 1.6.0
  * @author Object.NET, Inc.
  * @copyright Copyright 2008-2018 Object.NET, Inc.
- * @compiler Bridge.NET 16.8.2
+ * @compiler Bridge.NET 16.8.3
  */
 Bridge.assembly("Newtonsoft.Json.Tests", function ($asm, globals) {
     "use strict";
