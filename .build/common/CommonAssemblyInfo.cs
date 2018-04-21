ca35e966abef304ae3d3995f2f53d999ef353975
Fabrício Zimmerer Murta <fabricio@object.net>
2018-04-20 23:14:13 -0300

    Updates version to 1.7.0 and Bridge references to 17.0.0.

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.7.0")]
 [assembly: AssemblyFileVersion("1.7.0")]
-[assembly: AssemblyInformationalVersion("1.7.0-bridge17-0-0-beta")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2018 Object.NET, Inc.")]
