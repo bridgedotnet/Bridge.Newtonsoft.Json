0cdd47cd899bb3bd7fa6cbd8384e5a20b540fa1c
Fabrício Zimmerer Murta <fabricio@object.net>
2018-07-05 12:29:33 -0300

    Adds comments on unit tests to issue 112.

--- Tests/Newtonsoft.Json.Tests/Issues/0100/Case112.cs
+++ Tests/Newtonsoft.Json.Tests/Issues/0100/Case112.cs
@@ -1,21 +1,25 @@
 using Bridge.Test.NUnit;
-using System;
-using System.Collections.Generic;
 
 namespace Newtonsoft.Json.Tests.Issues
 {
+    /// <summary>
+    /// Ensures serialization of Long types works.
+    /// </summary>
     [Category("Issues")]
     [TestFixture(TestNameFormat = "#112 - {0}")]
     public class Case112
     {
+        /// <summary>
+        /// Creates a constant long then checks if it can be serialized.
+        /// </summary>
         [Test]
         public static void TestInt64()
         {
             long a = 19L;
             var serialized = JsonConvert.SerializeObject(a);
-            Assert.True((object)serialized is string);
-            Assert.AreEqual(2, serialized.Length);
-            Assert.AreEqual("19", serialized);
+            Assert.True((object)serialized is string, "Type long variables' SerializeObject() result is a string.");
+            Assert.AreEqual(2, serialized.Length, "The string length matches the number of digits in the variable.");
+            Assert.AreEqual("19", serialized, "The serialized string matches the expected value.");
         }
     }
 }
