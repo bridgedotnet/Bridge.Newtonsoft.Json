bcf7a985267b1193b31bc780436d2b8e2543f7e1
Fabrício Zimmerer Murta <fabricio@object.net>
2018-07-05 13:54:06 -0300

    Expands and comments on unit tests to issue 121.

--- Tests/Newtonsoft.Json.Tests/Issues/0100/Case121.cs
+++ Tests/Newtonsoft.Json.Tests/Issues/0100/Case121.cs
@@ -1,16 +1,18 @@
 using Bridge.Test.NUnit;
-using System;
-using System.Collections.Generic;
 
 namespace Newtonsoft.Json.Tests.Issues
 {
     /// <summary>
-    /// 
+    /// Ensures a nullable bool does not serialize to 'null' when it is
+    /// 'false'.
     /// </summary>
     [Category("Issues")]
     [TestFixture(TestNameFormat = "#121 - {0}")]
     public class Case121
     {
+        /// <summary>
+        /// A class with a series of nullable bool to be subject to the test.
+        /// </summary>
         public class TestDto
         {
             public bool? A { get; set; }
@@ -18,6 +20,11 @@ namespace Newtonsoft.Json.Tests.Issues
             public bool? C { get; set; }
         }
 
+        /// <summary>
+        /// Gives the three different possible values to the three nullable
+        /// booleans in the test class and ensures they are serialized forth
+        /// and back to the correct values.
+        /// </summary>
         [Test]
         public static void TestNullable()
         {
@@ -27,13 +34,15 @@ namespace Newtonsoft.Json.Tests.Issues
             test.C = null;
 
             var serialized = JsonConvert.SerializeObject(test);
-            Assert.AreEqual("{\"A\":true,\"B\":false,\"C\":null}", serialized);
+            Assert.AreEqual("{\"A\":true,\"B\":false,\"C\":null}", serialized, "Serializing nullable booleans results in a string that reflects the corresponding values.");
 
             var deserialized = JsonConvert.DeserializeObject<TestDto>(serialized);
 
-            Assert.True(deserialized.A.Value);
-            Assert.False(deserialized.B.Value);
-            Assert.Null(deserialized.C);
+            Assert.True(deserialized.A == test.A && deserialized.B == test.B && deserialized.C == test.C,
+                "Deserializing back results in equivalence between the object values.");
+            Assert.True(deserialized.A.Value, "Nullable boolean set to 'true' serializes back and forth to 'true'.");
+            Assert.False(deserialized.B.Value, "Nullable boolean set to 'false' serializes back and forth to 'false'.");
+            Assert.Null(deserialized.C, "Nullable boolean set to 'null' (or not set at all) serializes back and forth to its 'null' correspondence.");
         }
     }
 }
