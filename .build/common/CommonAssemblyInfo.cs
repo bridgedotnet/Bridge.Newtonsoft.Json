da06a7deaadc4c9ae316777391a16149e09efcb3
Fabrício Zimmerer Murta <fabricio@object.net>
2018-07-11 18:08:03 -0300

    Updates Bridge.Newtonsoft.Json version to 1.9.0 and Bridge to 17.2.0.

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.8.0")]
-[assembly: AssemblyFileVersion("1.8.0")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyInformationalVersion("1.9.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2018 Object.NET, Inc.")]
